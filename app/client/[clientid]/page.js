44eed4bf17f4a2b46eb7d0e519f2c7a747415a0c
RuslanGon <ruslan441006@gmail.com>
2024-10-09 19:15:38 +0300

    wer

--- app/client/[clientid]/page.js
+++ app/client/[clientid]/page.js
@@ -9,12 +9,4 @@ const page = ({params}) => {
   )
 }
 
-export async function generateStaticParams() {
-  const clients = [{ clientId: '1' }, { clientId: '2' }, { clientId: '3' }];
-
-  return clients.map(client => ({
-    clientId: client.clientId,
-  }));
-}
-
 export default page
--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,27 +1,36 @@
-import Details from '@/app/components/Details.js'
+import Details from '@/app/components/Details.js';
 
 export async function generateMetadata({ params }) {
     const post = await fetchPostById(params.id);
     return {
-      title: post.title,
-      description: post.body,
+        title: post.title,
+        description: post.body,
     };
-  }
-
-async function fetchPostById (id) {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
-    const result = await response.json()
-    return result
-  }
-
-const Post = async ({params: {id}}) => {
-  const post = await fetchPostById(id)
-
-  return (
-    <div className="post">
-       <Details post={post}/>
-    </div>
-  )
 }
 
-export default Post
+async function fetchPostById(id) {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
+    const result = await response.json();
+    return result;
+}
+
+const Post = async ({ params: { id } }) => {
+    const post = await fetchPostById(id);
+
+    return (
+        <div className="post">
+            <Details post={post} />
+        </div>
+    );
+};
+
+export async function generateStaticParams() {
+    // Здесь мы определяем статические параметры для генерации страниц постов
+    const postIds = ['1', '2', '3']; // Замените на реальные ID постов, которые вы хотите использовать
+
+    return postIds.map(id => ({
+        id: id, // Используем то же имя параметра, что и в маршруте
+    }));
+}
+
+export default Post;
