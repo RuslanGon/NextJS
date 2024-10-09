c1bf5d3a0897213f168b2c7615b440852582abbb
RuslanGon <ruslan441006@gmail.com>
2024-10-09 17:45:09 +0300

    wer

--- app/about/page.js
+++ app/about/page.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>About page</div>
+    <div>
+        <h3>About page</h3>
+    </div>
   )
 }
 
--- app/layout.js
+++ app/layout.js
@@ -2,16 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header.js";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -21,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body >
         <Header />
-        {children}
+        <div className="container">{children}</div>
       </body>
     </html>
   );
