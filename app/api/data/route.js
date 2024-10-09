72dcd311010b1b2991bb5b2c60cc2ba4bcb994d0
RuslanGon <ruslan441006@gmail.com>
2024-10-09 18:53:12 +0300

    wer

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -1,3 +1,16 @@
-export async function GET(request){
-    return new Response('hello Lena')
+export async function GET(request) {
+    const data = {
+        name: 'Ruslan',
+        age: 37,
+        gender: 'male'
+    }
+return new Response(JSON.stringify(data))
 }
+
+export async function POST(request) {
+return new Response(request.body)
+}
+
+export async function DELETE(request) {
+    return new Response(request.body)
+    }
